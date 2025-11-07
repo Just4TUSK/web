07e8460eda23878fc3e2e8670e8fdd8027fc99c4
Just4TUSK <dimaradoms11@gmail.com>
2025-11-07 15:36:49 +0200

    Lb8: added employyes.js, template in about.html, employeeCard, aboutPage, connected them in main.js + styles fixes and added employye-card style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import './styles/main.scss';
-import './styles/tw.css';
+import '@/styles/tw.css';
+import '@/styles/main.scss';
 
-import {initThemeToggle} from "@/js/common/theme.js";
-initThemeToggle()
+import { initThemeToggle } from '/src/js/common/theme.js';
+import { initAbout } from '/src/js/app/aboutPage.js';
+
+initThemeToggle();
+
+let cleanupFunction = () => {};
+
+initAbout()
+    .then(abortFn => {
+        cleanupFunction = abortFn;
+        console.log('Ініціалізація About Page завершена. Функція скасування збережена.');
+    })
+    .catch(error => {
+        console.error('Помилка при ініціалізації About Page:', error);
+    });
+
+// cleanupFunction();
